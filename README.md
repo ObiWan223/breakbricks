0a8831ad4a9c4bde8bfbbcd01b8dbee93a35b478
ObiWan223 <54301174+ObiWan223@users.noreply.github.com>
2024-10-16 12:31:51 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# creatingrandomballs
+# p5.play-boilerplate
+Boiler plate for p5.play
--- sketch.js
+++ sketch.js
@@ -1,54 +1,24 @@
-var ball1; // Define the game ball.
-var paddle1; // Define the first paddle.
-var paddle2; // Define the second paddle.
+//var ball, ballImg;
+let blocks = [];
 
-var redCol = 0
-var greenCol = 0;
-var blueCol = 0;
+function preload() {
+  ballImg = loadImage('ball.jpg');
+  trexAnimation = loadAnimation('trex_1.png', 'trex_2.png', 'trex_3.png');
+}
 
 function setup() {
-  createCanvas(400, 400); // Create the canvas.
-  ball1 = new Ball(); // Assign a new ball from the 'Ball' class.
-  paddle1 = new Paddle(); // Assign a new paddle from the 'Paddle' class.
-  paddle2 = new Paddle(); // Assign a new paddle from the 'Paddle' class.
+  createCanvas(800, 400);
+  //ball = createSprite(400, 200, 50, 50);
+  //ball.addImage(ballImg);
+  //ball.scale = 0.25;
 }
 
 function draw() {
-  background(redCol, greenCol, blueCol); // Set the background color to black.
-  
-  // Paddle 1 properties.
-  fill(255,0,0); // Set color of the left ball to red.
-  paddle1.x = 0; // Vertical position.
-  paddle1.y = 175; // Horizontal position.
-  paddle1.display(); // Show the left paddle.
-
-  // Paddle 2 properties.
-  fill(0,0,255); // Set color of the right paddle to blue.
-  paddle2.x = 385; // Set the horizontal position.
-  paddle2.y = mouseY; // Set the vertical position to be the mouseY position.
-  paddle2.display(); // Show the right paddle.
-  
-  // Ball properties.
-  fill(255,255,255); // Set color of the ball to white.
-  ball1.display(); // Display the game ball.
-  
-  moveBall();
+  background(255,255,255);  
+  drawSprites();
 }
 
-// Function that sets the ball movement.
-function moveBall() {
-  ball1.x = ball1.x + ball1.speed; // Set the initial speed.
-  text("ball x :" + ball1.x + ". ball y : " + ball1.y, 50, 50) // Debug.
-  text("paddle x :" + paddle2.x + ". paddle y : " + paddle2.y, 50, 70) // Debug.
-    if(ball1.x > width) { //  If the ball touches the right edge of the canvas. Change the background color.
-      redCol = 60;
-      greenCol = 234;
-      blueCol = 115;
-      ball1.speed = ball1.speed - 5; // Also make the ball move the opposite way.
-    } else if(ball1.x < 0) { // If the ball touches the left edge of the canvas. Change the background color.
-      redCol = 180;
-      greenCol = 30;
-      blueCol = 240;
-      ball1.speed = ball1.speed +5; // Also make the ball move the opposite way.
-    }
-  }
+function mouseClicked() {
+  trex = createSprite(400, 200, 50, 50);
+  trex.addAnimation("running", trexAnimation);
+}
